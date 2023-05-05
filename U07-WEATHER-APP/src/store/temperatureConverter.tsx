01fe11d85e95aff008ba86473af5011d1276da47
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-05-05 14:00:57 +0200

    commit

--- U07-WEATHER-APP/src/store/temperatureConverter.tsx
+++
@@ -1,8 +0,0 @@
-/* export function convertTemperature(temperature: any, toFarenheit: boolean) {
-  if (toFarenheit) {
-    return temperature * 1.8 + 32;
-  } else {
-    return temperature;
-  }
-}
- */
